42f7fda211a86f153387f7c7e082bb559a8231ee
Htetaungshine11 <newbtcearner@gmail.com>
2021-09-04 01:05:15 +0630

    Upload to Gitbhub

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import settings as s
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%t%(%2e1!covu50bikv@#js3114s3-7ntvvt^9@6x01evch_=j'
+
+SECRET_KEY = s.secretkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
